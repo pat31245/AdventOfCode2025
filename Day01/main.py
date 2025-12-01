8feb3ce9a1bcbc16079a36ff9a21da18a665798a
pat31245 <pa+github@die-drexlers.de>
2025-12-01 11:48:25 +0100

    day 1 finished

---
+++ Day01/main.py
@@ -0,0 +1,19 @@
+
+dial = 50
+zero_count = 0
+
+with open("input.txt") as f:
+    l = [line.rstrip() for line in f]
+    for line in l:
+        if line.startswith("L"):
+            dial -= int(line.lstrip("L"))
+        elif line.startswith("R"):
+            dial += int(line.lstrip("R"))
+
+        dial = dial % 100
+        if dial == 0:
+            zero_count += 1
+            print("0 count erhöht")
+        print(dial)
+    print(dial)
+    print("password: " + str(zero_count))

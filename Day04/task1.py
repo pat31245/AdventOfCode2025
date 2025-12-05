ac77b334aa4dfc62a48b5d45554c33fad19605c8
pat31245 <github@pat31245.dev>
2025-12-05 01:40:26 +0100

    task 1 finished

---
+++ Day04/task1.py
@@ -0,0 +1,44 @@
+import os
+
+roll_field_char = "@"
+
+def createField(fileContent: list[str]):
+    field = []
+    for line in fileContent:
+        # append chars as list
+        field.append([char for char in line.strip()])
+    return field
+
+def posValid(field: list[list[int]], x: int, y: int):
+    return (x < len(field[0]) and y < len(field) and x >= 0 and y >= 0)
+
+def adjacentRollsCount(field: list[list[str]], x: int, y: int):
+    count = 0
+    for x_offset in range(-1, 2, 1):
+        for y_offset in range(-1, 2, 1):
+            # filter out the field itself
+            if x + x_offset == x and y + y_offset == y:
+                continue
+            if posValid(field, x + x_offset, y + y_offset):
+                if field[y + y_offset][x + x_offset] == roll_field_char:
+                    count += 1
+    return count
+    
+for name in os.listdir("."):
+    # only iterate through .txts
+    if not name.endswith(".txt"):
+        continue
+    
+    with open(name) as f:
+        field = createField(f.readlines())
+        rolls_removable = 0
+        
+        # iterate over all lines and check for each paper roll if it can be picked up
+        for y in range(len(field)):
+            for x in range(len(field[0])):
+                if field[y][x] == roll_field_char:
+                    if adjacentRollsCount(field, x, y) < 4:
+                        rolls_removable += 1
+
+        # print out file name and amount of rolls that can be picked up
+        print(f"file: {name}; {rolls_removable} rolls can be picked up")
